696b07e636d5f38bfa97556a427b29ecc8195ede
Sadab Alif <101154563+Zeholygoat@users.noreply.github.com>
2026-05-02 00:36:42 +0600

    Update compare.js

--- compare.js
+++ compare.js
@@ -3,14 +3,25 @@ import { banks } from './data.js';
 
 const t = document.getElementById("table");
 
-t.innerHTML = "<tr><th>Bank</th><th>FDR</th><th>Tech</th><th>Cards</th><th>ATM</th></tr>";
+t.innerHTML = `
+<tr>
+<th>Bank</th>
+<th>FDR</th>
+<th>Tech</th>
+<th>Cards</th>
+<th>ATM</th>
+<th>Best For</th>
+</tr>
+`;
 
 banks.forEach(b=>{
-t.innerHTML += `<tr>
+t.innerHTML += `
+<tr>
 <td>${b.name}</td>
-<td>${b.fdr}</td>
-<td>${b.tech}</td>
-<td>${b.cards}</td>
-<td>${b.atm}</td>
+<td>${b.fdr}%</td>
+<td>${b.tech}/10</td>
+<td>${b.cards}/10</td>
+<td>${b.atm}/10</td>
+<td>${b.bestFor}</td>
 </tr>`;
 });
