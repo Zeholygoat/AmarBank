768cc2136f671bb60460e892ef84d5849049a59b
Sadab Alif <101154563+Zeholygoat@users.noreply.github.com>
2026-05-02 00:27:09 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,5 @@
+export const banks = [
+{ name:"City Bank", fdr:10.5, type:["pro","women"], features:["cards","premium"] },
+{ name:"DBBL", fdr:9.5, type:["student"], features:["atm","tech"] },
+{ name:"BRAC", fdr:10.2, type:["pro"], features:["service"] }
+];
