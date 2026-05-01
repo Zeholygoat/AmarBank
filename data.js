77a01223d35de4ff0bea7da694dfb100f46fa939
Sadab Alif <101154563+Zeholygoat@users.noreply.github.com>
2026-05-02 00:31:26 +0600

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-export const banks = [
-{ name:"City Bank", fdr:10.5, type:["pro","women"], features:["cards","premium"] },
-{ name:"DBBL", fdr:9.5, type:["student"], features:["atm","tech"] },
-{ name:"BRAC", fdr:10.2, type:["pro"], features:["service"] }
-];
