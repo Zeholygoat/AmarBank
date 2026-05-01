d440a11c7c9b5a5139d3b796f9912ccabca50a85
Sadab Alif <101154563+Zeholygoat@users.noreply.github.com>
2026-05-02 00:31:36 +0600

    Delete engine.js

--- engine.js
+++
@@ -1,7 +0,0 @@
-export function scoreBank(bank, user){
-let score = 0;
-if(bank.type.includes(user.role)) score += 2;
-if(bank.features.includes(user.priority)) score += 2;
-score += bank.fdr;
-return score;
-}
